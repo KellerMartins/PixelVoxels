644c49aca6919dd1631aea0d1ed6c3ec021742b5
Keller Clayderman <keller_clayderman@hotmail.com>
2017-09-14 14:45:08 -0300

    Organized logic and input calls and cleaned main

--- voxelLogic.h
+++ voxelLogic.h
@@ -7,7 +7,12 @@
 #include "voxelRenderer.h"
 #include "voxelLoader.h"
 #include "utils.h"
+
 #define POOLSIZE 2
+
+#define GetKey(n) keyboard_current[n]
+#define GetKeyDown(n) (keyboard_current[n] && !keyboard_last[n])
+#define GetKeyUp(n) (!keyboard_current[n] && keyboard_last[n])
 
 typedef enum ObjectType{PLAYER,BULLET,ENEMY}ObjectType;
 
@@ -19,11 +24,19 @@ typedef struct PoolObject{
     VoxelObject **objs;
     
 }PoolObject;
+void InputStart();
+void InputUpdate();
+void FreeInput();
+
+void GameStart();
+void GameUpdate();
 
 void InitializePool();
 void FreePool();
-void Spawn(unsigned int index,int x, int y, int z);
 void PoolUpdate();
+
+void Spawn(unsigned int index,int x, int y, int z);
+
 void MoveObject(VoxelObject *obj,float x, float y, float z,	VoxelObject **col,const int numCol,int damageColRadius,int damageObjRadius);
 void ExplodeAtPoint(VoxelObject *obj,int x, int y, int z,int radius);
 VoxelObject **VoxelPointerArrayUnion(int numberOfPointers,int totalPointerSize,...);
